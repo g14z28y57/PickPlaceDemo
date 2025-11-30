013db71488133f596ead68c950986f3ea8bf8f95
AnarchistKnight <zhengyang219@gmail.com>
2025-11-30 23:12:02 +0800

    update

--- model.py
+++ model.py
@@ -79,13 +79,17 @@ class BottleneckBlock(nn.Module):
 
 
 class MLP(nn.Module):
-    def __init__(self, in_dim, h_dim, num_layers):
+    def __init__(self, in_dim, h_dim, out_dim, num_layers):
         super().__init__()
-        self.layers = nn.ModuleList([BottleneckBlock(in_dim, h_dim) for _ in range(num_layers)])
+        self.in_proj = nn.Linear(in_dim, h_dim)
+        self.layers = nn.ModuleList([BottleneckBlock(h_dim, h_dim * 2) for _ in range(num_layers)])
+        self.out_proj = nn.Linear(h_dim, out_dim)
 
     def forward(self, x):
+        x = self.in_proj(x)
         for layer in self.layers:
             x = layer(x)
+        x = self.out_proj(x)
         return x
 
 
@@ -93,23 +97,20 @@ class VisionActionModel(nn.Module):
     def __init__(self):
         super().__init__()
         d_model = 16
+        d_feature = 32 * 3 + 5
         self.image_encoder = ResNet()
-        self.feature_projector = nn.Linear(32 * 3 + 5, d_model)
-        self.feature_fuser = MLP(in_dim=d_model, h_dim=d_model*2, num_layers=2)
-        self.position_delta_predictor = nn.Sequential(nn.Linear(d_model, 3), nn.Tanh())
-        self.next_catch_predictor = nn.Sequential(nn.Linear(d_model, 1), nn.Sigmoid())
-        self.next_task_predictor = nn.Sequential(nn.Linear(d_model, 1), nn.Sigmoid())
+        self.pos_delta_head = MLP(in_dim=d_feature, h_dim=d_model, out_dim=3, num_layers=3)
+        self.next_catch_head = MLP(in_dim=d_feature, h_dim=d_model, out_dim=1, num_layers=3)
+        self.next_task_head = MLP(in_dim=d_feature, h_dim=d_model, out_dim=1, num_layers=3)
 
     def forward(self, img1, img2, img3, state):
         f1 = self.image_encoder(img1)
         f2 = self.image_encoder(img2)
         f3 = self.image_encoder(img3)
         f = torch.concatenate([f1, f2, f3, state], dim=-1)
-        f = self.feature_projector(f)
-        f = self.feature_fuser(f)
-        position_delta = self.position_delta_predictor(f)
-        position_delta = F.normalize(position_delta, p=2, dim=1)
-        next_catch = self.next_catch_predictor(f)
-        next_task = self.next_task_predictor(f)
-        next_state = torch.concatenate([position_delta, next_catch, next_task], dim=-1)
+        pos_delta = nn.Tanh()(self.pos_delta_head(f))
+        pos_delta = F.normalize(pos_delta, p=2, dim=1)
+        next_catch = nn.Sigmoid()(self.next_catch_head(f))
+        next_task = nn.Sigmoid()(self.next_task_head(f))
+        next_state = torch.concatenate([pos_delta, next_catch, next_task], dim=-1)
         return next_state
