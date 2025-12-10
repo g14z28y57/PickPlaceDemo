57aeced8dd1d67b2f63176fbd677adeeea533fc6
HyperSphere <145274399+g14z28y57@users.noreply.github.com>
2025-12-10 17:51:04 +0800

    Fix matplotlib installation syntax in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 pip install numpy==1.26.3
 pip install opencv-python==4.9.0.80
-pip install matplotlib 3.10.7
+pip install matplotlib==3.10.7
 pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
 # find and download .whl from https://download.blender.org/pypi/bpy/
 pip install bpy-4.5.1-cp311-cp311-win_amd64.whl
